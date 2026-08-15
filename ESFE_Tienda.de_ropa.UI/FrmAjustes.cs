a8c97f74dffdc08f3e7eb432f73f1bad155e36bf
Alex20 <Stevansegura55@gmail.com>
2026-08-14 21:06:42 -0600

    Actualizar tarea

---
+++ ESFE_Tienda.de_ropa.UI/FrmAjustes.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ESFE_Tienda.de_ropa.UI
+{
+    public partial class FrmAjustes : Form
+    {
+        public FrmAjustes()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
