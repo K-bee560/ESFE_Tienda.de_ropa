ffefa163c134d3b723e89c5254569630a75f1605
Anderson Cisneros <diegoandercis@gmail.com>
2026-08-24 08:56:01 -0600

    Actualizar

--- ESFE_Tienda.de_ropa.UI/FrmEstado.Designer.cs
+++ ESFE_Tienda.de_ropa.UI/FrmEstado.Designer.cs
@@ -52,6 +52,7 @@
             button1.TabIndex = 0;
             button1.Text = "Buscar ";
             button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
             // 
             // textBox1
             // 
--- ESFE_Tienda.de_ropa.UI/FrmEstado.cs
+++ ESFE_Tienda.de_ropa.UI/FrmEstado.cs
@@ -14,5 +14,10 @@ namespace ESFE_Tienda.de_ropa.UI
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
