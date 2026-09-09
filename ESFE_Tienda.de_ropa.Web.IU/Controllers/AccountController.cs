39182d49776fcb6e7ccddcb2a2409a26791bdc10
Anderson Cisneros <diegoandercis@gmail.com>
2026-09-09 09:49:27 -0600

    actualizar

---
+++ ESFE_Tienda.de_ropa.Web.IU/Controllers/AccountController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ESFE_Tienda_de_ropa.Web.IU.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: /Account/Login
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        // POST: /Account/Login
+        [HttpPost]
+        public IActionResult Login(string email, string password)
+        {
+            // Validación de prueba
+            if (email == "admin@tienda.com" && password == "123456")
+            {
+                return RedirectToAction("Index", "Producto");
+            }
+
+            ViewBag.Error = "Correo o contraseña incorrectos.";
+            return View();
+        }
+    }
+}
