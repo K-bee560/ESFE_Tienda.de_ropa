774250cf94399f4061b16517bf3a69402a76a4a0
Anderson Cisneros <diegoandercis@gmail.com>
2026-09-09 08:04:13 -0600

    Actualizar

---
+++ ESFE_Tienda.de_ropa.Web.IU/Controllers/ProductoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ESFE_Tienda_de_ropa.Web.IU.Controllers
+{
+    public class ProductoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
