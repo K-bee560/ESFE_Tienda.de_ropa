18a1849f38ee875258031dbe9ad7b9c8ffec7287
Anderson Cisneros <diegoandercis@gmail.com>
2026-09-10 10:20:34 -0600

    actualizar

--- ESFE_Tienda.de_ropa.Web.IU/wwwroot/script.js
+++ ESFE_Tienda.de_ropa.Web.IU/wwwroot/script.js
@@ -7,6 +7,7 @@
     const contenedorResultado = document.querySelector('#contenedorResultado');
     const totalTexto = document.querySelector('#totalTexto');
     const mensajeEstado = document.querySelector('#mensajeEstado');
+    const badgeDescuento = document.querySelector('#badgeDescuento');
 
     btnCalcular.addEventListener('click', () => {
 
@@ -24,17 +25,20 @@
         contenedorResultado.className = 'resultado-box';
 
         if (subtotal >= 100) {
-            descuento = 0.20; // 20% de descuento
+            descuento = 0.20;
             contenedorResultado.classList.add('excelente');
             mensajeEstado.textContent = '🌟 ¡Super Descuento Claret! Tienes un 20% OFF por compra mayor a $100.';
+            badgeDescuento.textContent = '20% OFF';
         } else if (subtotal >= 50) {
-            descuento = 0.10; // 10% de descuento
+            descuento = 0.10;
             contenedorResultado.classList.add('aprobado');
             mensajeEstado.textContent = '👍 ¡Promoción Claret! Tienes un 10% OFF por compra mayor a $50.';
+            badgeDescuento.textContent = '10% OFF';
         } else {
             descuento = 0;
             contenedorResultado.classList.add('reforzar');
             mensajeEstado.textContent = '🏷️ Compra estándar. ¡Agrega más prendas para conseguir descuento!';
+            badgeDescuento.textContent = '0% OFF';
         }
 
         const totalFinal = subtotal - (subtotal * descuento);
